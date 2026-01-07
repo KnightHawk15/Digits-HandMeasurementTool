03d79687d3fda8b7ae32d30d9825d6a1907852b5
epeters <15esp3@queensu.ca>
2026-01-06 22:02:12 -0500

    Restoring files

---
+++ Digits-HandMeasurementTool/src/App.js
@@ -0,0 +1,22 @@
+import HandTracker from './components/HandTracker/HandTracker';
+import Home from './components/Home/Home';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home />}>
+          <Route path='/hand-tracker' element={<HandTracker />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
